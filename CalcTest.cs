76dbe1f1fc3169681ee477d12c3b2c08ea97e5e0
DeLaGowski <102720363+DeLaGowski@users.noreply.github.com>
2022-04-02 15:43:30 +0200

    Add files via upload

---
+++ CalcTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Calc;
+using Moq;
+
+namespace CalcTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalcTest()
+        {
+
+            // arrange
+            Mock<ICalc> mock = new();
+
+            mock.Setup(x => x.GetResult())
+                .Returns(666);
+
+            Calcc kalkulator = new();
+
+
+            // act
+            var result = kalkulator.Add(5, 5);
+
+
+            // assert
+            Assert.AreEqual(666,result);
+        }
+    }
+}
